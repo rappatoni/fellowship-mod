7c931a9a8b2b3e89f5e2eaa233fd7fec92ae341f
Max Rapp <max.rapp@fau.de>
2025-07-09 14:40:57 +0200

    Modify adapter for focussed undercut. Next step: implement grafting on the argument body level.

--- fsp/tags/fellowship-0.1.0/wrapper.py
+++ fsp/tags/fellowship-0.1.0/wrapper.py
@@ -769,7 +769,7 @@ class Argument:
 
         #invert the conclusion:
         issue = self.conclusion.strip('~')
-        adapter_arg = Argument(self.prover, f'undercuts_on_{other_argument.assumptions[attacked_assumption]["prop"]}', issue, [f'cut (~{issue}) undercut.', f'next.', f'elim.'])
+                adapter_arg = Argument(self.prover, f'undercuts_on_{other_argument.assumptions[attacked_assumption]["prop"]}', issue, [f'cut ({issue}) alt.', f'cut ({issue}) aff', f'next', f'axiom alt', f'cut (~{issue}) aff', f'next', f'elim'])
         adapter_arg.execute()
         adapted_argument = adapter_arg.chain(other_argument)
         print("ADAPTED")
